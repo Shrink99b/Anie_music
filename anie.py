929b9b63bac47123a328debb4595c42988e5dfea
Shrink99b <115853073+Shrink99b@users.noreply.github.com>
2023-03-03 09:27:00 +0530

    Update anie.py

--- anie.py
+++ anie.py
@@ -6,14 +6,12 @@ from config import BOT_USERNAME as cli
 
 async def start_bot():
     LOGS.info("[INFO]: BOT & ANIEUSERBOT CLIENT STARTED !!")
-    await calls.start()
     LOGS.info("[INFO]: PY-TGCALLS CLIENT STARTED !!")
     await user.join_chat("aniebotsupports")
     await user.join_chat("aniebots")
     await user.send_message(f"{cli}", "/start")
     await idle()
     LOGS.info("[INFO]: BOT & USERBOT STOPPED !!")
-    await bot.stop()
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(start_bot())
